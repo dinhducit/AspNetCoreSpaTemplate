cdcb0a147ae4befce10ee1236201441c7cdd1785
ducnguyenvn <dinhducit@gmail.com>
2019-09-29 08:22:38 +0700

    #30min to fix bug init data - missing save file

--- AspnetCoreSPA/Domain/ApplicationDbContext.cs
+++ AspnetCoreSPA/Domain/ApplicationDbContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AspnetCoreSPATemplate.Domain.Models;
+﻿using AspnetCoreSPATemplate.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace AspnetCoreSPATemplate.Domain
